c867324de1291e17638f69969e8b23ffef078169
zhaorunxiao <zhaorunxiao@sics.ac.cn>
2025-10-23 21:53:48 +0800

    optimize color mode mgr

--- rnsoulmate/App.tsx
+++ rnsoulmate/App.tsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from 'react';
-import { SafeAreaView, StyleSheet, ColorSchemeName, Button } from 'react-native';
+import { SafeAreaView, StyleSheet} from 'react-native';
 import { useColorScheme as useDeviceColorMode } from 'react-native';
 import { Platform } from 'react-native';
 
 import { LoggerFactory } from './src/internal/logger/logger';
-import { LoggingView } from './src/internal/logger/logging-view';
 
 import MainText from './src/components/main-text';
-import { ColorMode } from './src/internal/system';
+import { ColorModeManager } from './src/components/ui/color-mode-manager';
 
 import { colorMode as settingColorMode } from './app.json';
 
 const logger = LoggerFactory.getLogger('App');
+
+// 创建颜色模式管理器
+const colorModeManager = new ColorModeManager(settingColorMode);
 
 const App = () => {
   // 获取当前操作系统
@@ -20,33 +22,21 @@ const App = () => {
 
   // 获取当前设备颜色模式
   const deviceColorMode = useDeviceColorMode();
-  logger.info(`Device color mode: ${deviceColorMode}`);
 
   // 获取用户颜色模式（当前颜色模式为 auto 时，监听设备颜色，否则使用用户颜色模式）
-  const [colorMode, setColorMode] = useState(userColorMode(deviceColorMode));
+  const [colorMode, setColorMode] = useState(colorModeManager.current());
   useEffect(()=>{
-    logger.info('Device color mode changed to ' + deviceColorMode)
-    setColorMode(userColorMode(deviceColorMode));
+    setColorMode(colorModeManager.current());
   }, [deviceColorMode])
-  logger.info(`User color mode: ${colorMode}`);
 
-  const switchColorMode = () => {
-    logger.info(`Switching color mode from ${colorMode}`)
-    if (settingColorMode !== 'auto') {
-      if (colorMode === ColorMode.Dark) {
-        logger.info(`Switching color mode to ${ColorMode.Light}`);
-        setColorMode(ColorMode.Light);
-      } else {
-        logger.info(`Switching color mode to ${ColorMode.Dark}`);
-        setColorMode(ColorMode.Dark);
-      }
-    }
-  }
+  const switchColorMode = () => setColorMode(colorModeManager.switch())
+  
+  // 注册监听 Ctrl+M 键，切换颜色模式
   useEffect(()=>{
     if (Platform.OS === 'web') {
+      logger.info('Ctrl+M to switch color mode');
       const handleKeyDown = (event: KeyboardEvent) => {
         if (event.ctrlKey && event.key === 'm') {
-          logger.info('Ctrl+M pressed');
           switchColorMode();
         }
       };
@@ -55,35 +45,22 @@ const App = () => {
         window.removeEventListener('keydown', handleKeyDown);
       };
     }
-  }, [switchColorMode])
+  }, [])
 
   return (
     <SafeAreaView style={[styles.body, styles[colorMode]]}>
       <MainText text='Hello, World!' colorMode={colorMode} />
       <MainText text={"My React Native App is running on the " + currentPlatform  + "!"} colorMode={colorMode} />
       <MainText text={"Current color mode is " + colorMode  + "!"} colorMode={colorMode} />
-      <Button onPress={switchColorMode} title="Switch" />
-      <LoggingView />
     </SafeAreaView>
   );
 };
 
-function userColorMode(deviceColorMode:ColorSchemeName): ColorMode {
-  if (settingColorMode === ColorMode.Dark) {
-    return ColorMode.Dark;
-  } else if (settingColorMode === ColorMode.Light) {
-    return ColorMode.Light;
-  } else {
-    if (deviceColorMode === ColorMode.Dark) {
-      return ColorMode.Dark;
-    } else {
-      return ColorMode.Light;
-    }
-  }
-}
-
 const styles = StyleSheet.create({
   body: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   }, 
   light: {
     backgroundColor: '#FFFFFF', // 白色背景，适合浅色模式
---
+++ rnsoulmate/src/components/ui/color-mode-manager.ts
@@ -0,0 +1,81 @@
+/**
+ * 系统 color mode 管理器
+ * 管理 UI 呈现 dark / light 模式
+ */
+
+import { Appearance } from "react-native";
+
+import { LoggerFactory } from "../../internal/logger/logger";
+
+const logger = LoggerFactory.getLogger("ColorModeManager");
+
+enum ColorMode {
+  Light = "light",
+  Dark = "dark",
+}
+
+enum ColorModeAuto {
+  Light = "light",
+  Dark = "dark",
+  Auto = "auto",
+}
+
+class ColorModeManager {
+  // 用户默认定义的模式
+  private userDefaultColorMode: ColorModeAuto;
+  private currentColorMode: ColorMode;
+
+  constructor(userDefaultColorMode: string) {
+    logger.info(`new ColorModeManager with default color mode: ${userDefaultColorMode}`)
+    switch (userDefaultColorMode) {
+      case ColorMode.Light:
+        this.userDefaultColorMode = ColorModeAuto.Light;
+        break;
+      case ColorMode.Dark:
+        this.userDefaultColorMode = ColorModeAuto.Dark;
+        break;
+      default:
+        this.userDefaultColorMode = ColorModeAuto.Auto;
+    }
+    this.currentColorMode = this.current();
+    logger.info(`current color mode: ${this.currentColorMode}`)
+  }
+
+  // 获取当前应该呈现的颜色模式
+  current(): ColorMode {
+    switch (this.userDefaultColorMode) {
+      case ColorModeAuto.Light:
+        return ColorMode.Light;
+      case ColorModeAuto.Dark:
+        return ColorMode.Dark;
+      case ColorModeAuto.Auto:
+        const deviceColorMode = Appearance.getColorScheme();
+        logger.info(`device color mode: ${deviceColorMode}`)
+        switch (deviceColorMode) {
+          case "light":
+            return ColorMode.Light;
+          case "dark":
+            return ColorMode.Dark;
+          default:
+            return ColorMode.Light;
+        }
+    }
+  }
+
+  switch(): ColorMode {
+    if (this.userDefaultColorMode === ColorModeAuto.Auto) {
+      logger.info(`switch color mode from ${this.currentColorMode}`)
+      switch (this.currentColorMode) {
+        case ColorMode.Light:
+          this.currentColorMode = ColorMode.Dark;
+          break
+        case ColorMode.Dark:
+          this.currentColorMode = ColorMode.Light;
+          break
+      }
+    }
+    return this.currentColorMode;
+  }
+}
+
+export { ColorMode, ColorModeManager };
